19fdc6dc95d6fc5fac02a3cb95507282e44e46e0
p_emindu <p.emindu@directfn.com>
2025-04-11 13:23:03 +0530

    added OrderCancellationProcessor

---
+++ src/main/java/com/dfn/lsf/service/impl/OrderCancellationProcessor.java
@@ -0,0 +1,43 @@
+package com.dfn.lsf.service.impl;
+
+import com.dfn.lsf.model.requestMsg.CommonInqueryMessage;
+import com.dfn.lsf.repository.LSFRepository;
+import com.dfn.lsf.service.LsfCoreService;
+import com.dfn.lsf.service.MessageProcessor;
+import com.dfn.lsf.util.Helper;
+import com.dfn.lsf.util.LsfConstants;
+import com.dfn.lsf.util.NotificationManager;
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Not defined route found only processor created named orderCancellationProcessor
+ */
+@Service
+@RequiredArgsConstructor
+public class OrderCancellationProcessor implements MessageProcessor {
+
+    private static final Logger logger = LoggerFactory.getLogger(MurabahApplicationPersistProcessor.class);
+
+    private final Gson gson;
+    private final LSFRepository lsfRepository;
+    private final Helper helper;
+    private final NotificationManager notificationManager;
+    private final LsfCoreService lsfCoreService;
+
+    @Override
+    public String process(String request) {
+        CommonInqueryMessage basketCancellationRequest = new CommonInqueryMessage();
+        basketCancellationRequest.setReqType(LsfConstants.ORDER_BASKET_CANCELLATION);
+        String response = helper.handleOMSRequest(
+                helper.sendMessageToOms(gson.toJson(basketCancellationRequest))
+                      .toString(),
+                LsfConstants.HTTP_PRODUCER_OMS_REQ_HANDLER);
+        logger.debug("===========LSF : Order Basket Cancellation Request received & Sent to OMS , OMS Response :"
+                     + response);
+        return null;
+    }
+}
