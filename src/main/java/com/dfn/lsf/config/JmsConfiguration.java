b8d85583cded6b17b75ee0dc9b35cc9789a371c0
suranga.caldera <suranga.caldera@pythiasports.com>
2025-04-17 18:01:34 +0530

    updated some chagnes

---
+++ src/main/java/com/dfn/lsf/config/JmsConfiguration.java
@@ -0,0 +1,108 @@
+package com.dfn.lsf.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.jndi.JndiTemplate;
+
+import jakarta.jms.ConnectionFactory;
+import jakarta.jms.Queue;
+import javax.naming.NamingException;
+import java.util.Properties;
+
+/**
+ * JMS Configuration for JBoss/WildFly integration
+ * Reads configuration values from application.properties
+ */
+@Configuration
+@EnableJms
+public class JmsConfiguration {
+    
+    @Value("${spring.jms.jndi-name:java:/JmsXA}")
+    private String jmsJndiName;
+    
+    @Value("${lsf.jms.queue.to-lsf:java:/queue/TO_LSF_QUEUE}")
+    private String toLsfQueueName;
+    
+    @Value("${lsf.jms.initial-context-factory:org.jboss.naming.remote.client.InitialContextFactory}")
+    private String initialContextFactory;
+    
+    @Value("${lsf.jms.provider-url:http-remoting://localhost:8080}")
+    private String providerUrl;
+    
+    @Value("${lsf.jms.connection-factory-jndi-name:java:/JmsXA}")
+    private String connectionFactoryJndiName;
+    
+    /**
+     * Create a JNDI template for looking up JMS resources
+     */
+    @Bean
+    public JndiTemplate jndiTemplate() {
+        Properties jndiProps = new Properties();
+        jndiProps.setProperty(javax.naming.Context.INITIAL_CONTEXT_FACTORY, initialContextFactory);
+        jndiProps.setProperty(javax.naming.Context.PROVIDER_URL, providerUrl);
+        
+        return new JndiTemplate(jndiProps);
+    }
+    
+    /**
+     * Get JMS connection factory from JNDI
+     */
+    @Bean
+    @Primary
+    public ConnectionFactory connectionFactory() throws NamingException {
+        JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+        jndiObjectFactoryBean.setJndiTemplate(jndiTemplate());
+        jndiObjectFactoryBean.setJndiName(connectionFactoryJndiName);
+        jndiObjectFactoryBean.setResourceRef(true);
+        jndiObjectFactoryBean.setProxyInterface(ConnectionFactory.class);
+        jndiObjectFactoryBean.afterPropertiesSet();
+        return (ConnectionFactory) jndiObjectFactoryBean.getObject();
+    }
+    
+    /**
+     * Configure JMS listener container factory
+     */
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        return factory;
+    }
+    
+    /**
+     * Get the TO_LSF_QUEUE from JNDI
+     */
+    @Bean(name = "lsfQueue")
+    public Queue toLsfQueue() throws NamingException {
+        JndiObjectFactoryBean factoryBean = new JndiObjectFactoryBean();
+        factoryBean.setJndiTemplate(jndiTemplate());
+        factoryBean.setJndiName(toLsfQueueName);
+        factoryBean.setResourceRef(true);
+        factoryBean.afterPropertiesSet();
+        return (Queue) factoryBean.getObject();
+    }
+
+    @Bean
+    public Queue jmsQueue() throws NamingException {
+        JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+        jndiObjectFactoryBean.setJndiTemplate(jndiTemplate());
+        jndiObjectFactoryBean.setJndiName(jmsJndiName);
+        jndiObjectFactoryBean.setResourceRef(true);
+        jndiObjectFactoryBean.setProxyInterface(Queue.class);
+        jndiObjectFactoryBean.afterPropertiesSet();
+        return (Queue) jndiObjectFactoryBean.getObject();
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
+        JmsTemplate jmsTemplate = new JmsTemplate();
+        jmsTemplate.setConnectionFactory(connectionFactory);
+        return jmsTemplate;
+    }
+} 
---
+++ src/main/java/com/dfn/lsf/jms/LsfJmsListener.java
@@ -0,0 +1,118 @@
+package com.dfn.lsf.jms;
+
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import com.dfn.lsf.model.requestMsg.OMSQueueRequest;
+import com.dfn.lsf.service.impl.AccountUpdateProcessor;
+import com.dfn.lsf.service.impl.DepositWithdrawResponseHandlingProcessor;
+import com.dfn.lsf.service.impl.LsfOmsValidatorAbicProcessor;
+import com.dfn.lsf.service.impl.SettlementCalculationProcessor;
+import com.dfn.lsf.service.impl.UpdateOrderStatusProcessor;
+import com.dfn.lsf.util.LsfConstants;
+import com.google.gson.Gson;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * JMS Listener for receiving and processing messages from TO_LSF_QUEUE
+ * Replaces the AKKA-based implementation in the original system
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class LsfJmsListener {
+    
+    private final Gson gson;
+    private final AccountUpdateProcessor accountUpdateProcessor;
+    private final LsfOmsValidatorAbicProcessor lsfOmsValidatorProcessor;
+    private final UpdateOrderStatusProcessor updateOrderStatusProcessor;
+    private final SettlementCalculationProcessor settlementCalculationProcessor;
+    private final DepositWithdrawResponseHandlingProcessor depositWithdrawProcessor;
+    
+    /**
+     * Handles messages from the JBoss TO_LSF_QUEUE
+     * @param message Text message from JMS queue
+     */
+    @JmsListener(destination = "${lsf.jms.queue.to-lsf:TO_LSF_QUEUE}")
+    public void handleJmsMessage(String message) {
+        log.info("Received message from JMS queue: {}", message);
+        
+        try {
+            // Parse the message
+            OMSQueueRequest omsRequest = gson.fromJson(message, OMSQueueRequest.class);
+            
+            // Route based on message type
+            switch (omsRequest.getMessageType()) {
+                case LsfConstants.UPDATE_ORDER_STATUS_PROCESS:
+                    // Core processor handles order status updates, liquidations, deposits, withdrawals
+                    processOrderStatusUpdate(message, "updateOrderStatus");
+                    break;
+                    
+                case LsfConstants.APPROVE_ORDER_FOR_FTV:
+                case LsfConstants.APPROVE_WITHDRAW_FOR_FTV:
+                case LsfConstants.RIA_LOGOUT_RESPONSE: 
+                case LsfConstants.TRADE_HOLDING_UPDATE_RESPONSE:
+                    // OMS validator handles validation requests
+                    processWithValidator(message, "validateRequest");
+                    break;
+                    
+                case LsfConstants.EXCHANGE_ACCOUNT_DELETION_RESPONSE:
+                case LsfConstants.INVESTOR_ACCOUNT_CREATION_RESPONSE:
+                    // Account creation responses handled by core processor
+                    accountUpdateProcessor(message, "accountCreationResponse");
+                    break;
+                    
+                case LsfConstants.LIQUIDATION_SUCCESS_RESPONSE:
+                    processWithSettlementCalculation(message, "liquidationResponse");
+                    break;
+                    
+                case LsfConstants.DEPOSIT_SUCCESS_RESPONSE:
+                case LsfConstants.WITHDRAW_SUCCESS_RESPONSE:
+                    processWithDepositWithdraw(message, "depositWithdrawResponse");
+                    break;
+                    
+                default:
+                    log.warn("Unhandled message type: {}", omsRequest.getMessageType());
+            }
+        } catch (Exception e) {
+            log.error("Error processing message: {}", e.getMessage(), e);
+        }
+    }
+    
+    /**
+     * Process message using the LsfCoreProcessor
+     */
+    private void accountUpdateProcessor(String message, String subMessageType) {
+        log.info("Processing with core processor: {}", subMessageType);
+        accountUpdateProcessor.process(message);
+    }
+    
+    /**
+     * Process message using the LsfOmsValidatorAbicProcessor
+     */
+    private void processWithValidator(String message, String subMessageType) {
+        log.info("Processing with validator: {}", subMessageType);
+        lsfOmsValidatorProcessor.process(message);
+    }
+
+    private void processOrderStatusUpdate(String message, String subMessageType) {
+        log.info("Processing order status update: {}", subMessageType);
+        updateOrderStatusProcessor.process(message);
+    }
+
+    private void processWithSettlementCalculation(String message, String subMessageType) {
+        log.info("Processing with settlement calculation: {}", subMessageType);
+        settlementCalculationProcessor.process(message);
+    }
+
+    private void processWithDepositWithdraw(String message, String subMessageType) {
+        log.info("Processing with deposit/withdraw: {}", subMessageType);
+        depositWithdrawProcessor.process(message);
+    }
+
+} 
